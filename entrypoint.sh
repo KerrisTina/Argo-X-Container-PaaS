444a7709d5cc7eb5bdc21ebb6a6d6f45d7d82ac8
KerrisTina <68241507+KerrisTina@users.noreply.github.com>
2023-04-01 09:03:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'b8dd131c-7394-450e-da0e-ff6888567c96'}
 
 generate_config() {
   cat > config.json << EOF
